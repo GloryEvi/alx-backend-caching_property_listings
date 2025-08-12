4e606a609aefd7be0894426b02bfd7940896b809
GloryEvi <167655575+GloryEvi@users.noreply.github.com>
2025-08-12 16:30:20 +0100

    Implement property_list view with 15-minute Redis caching and URL mapping

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .models import Property
+
+@cache_page(60 * 15)  # Cache for 15 minutes
+def property_list(request):
+    """
+    View to return all properties with Redis caching for 15 minutes
+    """
+    properties = Property.objects.all()
+    
+    # Convert to list of dictionaries for JSON response
+    property_data = []
+    for property_obj in properties:
+        property_data.append({
+            'id': property_obj.id,
+            'title': property_obj.title,
+            'description': property_obj.description,
+            'price': str(property_obj.price),
+            'location': property_obj.location,
+            'created_at': property_obj.created_at.isoformat()
+        })
+    
+    return JsonResponse({
+        'properties': property_data,
+        'count': len(property_data)
+    })
